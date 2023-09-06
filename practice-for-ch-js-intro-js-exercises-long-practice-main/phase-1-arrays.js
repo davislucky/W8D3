59a90f2fbc06d20e5c3a64e2477d68cd09c61317
Queen Belle V. Dela Cruz <qb.dcruz@gmail.com>
2023-09-06 15:07:20 -0700

    Finish phase 1

--- practice-for-ch-js-intro-js-exercises-long-practice-main/phase-1-arrays.js
+++ practice-for-ch-js-intro-js-exercises-long-practice-main/phase-1-arrays.js
@@ -29,3 +29,36 @@ Array.prototype.twoSum = function() {
 
 // console.log([-1, 0, 2, -2, 1].twoSum())
 
+Array.prototype.transpose = function() {
+    let transposed = [];
+
+    for (let i = 0; i < this.length; i++) {
+        let subArr = this[i]
+        let transpSub = []
+        // console.log("subArr:",subArr)
+
+        for (let j = 0; j < subArr.length; j++) {
+            let ele = this[j][i];
+            // console.log("ele:",ele)
+
+            transpSub.push(ele)
+        }
+
+        transposed.push(transpSub)
+    }
+
+    return transposed
+}
+
+// console.log([[0, 1, 2], [3, 4, 5], [6, 7, 8]].transpose())
+
+// i = row, j = col ij
+// [[00, 01, 02]
+// [10, 11, 12]
+// [20, 21, 22]]
+
+// ji
+// [[00, 10, 20]
+// [01, 11, 21]
+// [02, 12, 22]]
+
