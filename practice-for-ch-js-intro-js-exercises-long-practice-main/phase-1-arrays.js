e9f46fb2314fa8b28ed54e96f5dcf5563260637c
davislucky <58579323+davislucky@users.noreply.github.com>
2023-09-06 14:45:40 -0700

    Monkeypatch uniq and twoSum to Array

--- practice-for-ch-js-intro-js-exercises-long-practice-main/phase-1-arrays.js
+++ practice-for-ch-js-intro-js-exercises-long-practice-main/phase-1-arrays.js
@@ -1,1 +1,31 @@
-// Your code here
+Array.prototype.uniq = function() {
+    let obj = {};
+
+    for (let i = 0; i < this.length; i++) {
+        obj[this[i]] = true;
+    }
+    return Object.keys(obj);
+}
+
+// console.log([1,2,2,3,3,3].uniq())
+
+Array.prototype.twoSum = function() {
+    let twoSums = {};
+    let pairs = [];
+
+    for (let i = 0; i < this.length; i++) {
+        let ele = this[i];
+        let complement = twoSums[-ele]
+        // console.log("current num:", ele)
+        // console.log("complement", complement)
+
+        if (complement !== undefined) {
+            pairs.push([complement, i]);
+        }
+        twoSums[ele] = i;
+    }
+    return pairs
+}
+
+// console.log([-1, 0, 2, -2, 1].twoSum())
+
