1c1ee6a7aecde5654d8165880eefccfcc2c99721
davislucky <58579323+davislucky@users.noreply.github.com>
2023-09-06 16:44:35 -0700

    Finish Phase 2

--- practice-for-ch-js-intro-js-exercises-long-practice-main/phase-2-enumerable.js
+++ practice-for-ch-js-intro-js-exercises-long-practice-main/phase-2-enumerable.js
@@ -15,6 +15,36 @@ Array.prototype.myMap = function(callback) {
   return map
 }
 
-const addTwo = (num) => num + 2;
-arr = [2, 5, 7];
-console.log(arr.myMap(addTwo))
+// const addTwo = (num) => num + 2;
+// arr = [2, 5, 7];
+// console.log(arr.myMap(addTwo))
+
+Array.prototype.myReduce = function(callback, initialValue) {
+  let acc = initialValue;
+
+  this.myEach(function(ele) {   
+    // if (initialValue === this[0]) {
+    //   acc = callback(this[1], ele)
+    // }
+    // else {
+      if (acc === undefined) {
+        acc = ele;
+      } else {
+        acc = callback(acc, ele);
+      }
+     
+    // }
+  })
+  return acc;
+}
+
+console.log([1, 2, 3].myReduce(function(acc, el) {
+  return acc + el;
+}));
+
+console.log([1, 2, 3].myReduce(function(acc, el) {
+  return acc + el;
+}, 25));
+
+
+
