1425ad4ec21f6436bf6717ff41bbdfe092cc898d
Queen Belle V. Dela Cruz <qb.dcruz@gmail.com>
2023-09-06 15:39:30 -0700

    Monkeypatched foreach and map

--- practice-for-ch-js-intro-js-exercises-long-practice-main/phase-2-enumerable.js
+++ practice-for-ch-js-intro-js-exercises-long-practice-main/phase-2-enumerable.js
@@ -1,1 +1,20 @@
-// Your code here
+Array.prototype.myEach = function (callback) {
+  for (let i = 0; i < this.length; i++) {
+    let ele = this[i];
+    callback(ele);
+  }
+}
+
+
+
+Array.prototype.myMap = function(callback) {
+  let map = [];
+
+  this.myEach( ele => map.push(callback(ele)) )
+
+  return map
+}
+
+const addTwo = (num) => num + 2;
+arr = [2, 5, 7];
+console.log(arr.myMap(addTwo))
